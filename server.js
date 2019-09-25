c1dfc288a870b9da449756cbb102522a57da8d48
Robert Wise <robcodehub@gmail.com>
2019-09-25 19:54:35 +0700

    added path

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const path = require('path');
+
 router.get('/', (res, req, next) => {
-  res.sendFile(__dirname, 'index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
