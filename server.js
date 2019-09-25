db280a9fffcdf16885ee9c865ffa18610e0fa742
Robert Wise <robcodehub@gmail.com>
2019-09-25 20:02:13 +0700

    updated server.js file

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const router = express.Router();
 
 const path = require('path');
 
+const app = express();
+
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
+
+const port = process.env.PORT || 3005;
+
+app.listen(port, ()=> console.log(`listening on port ${port}`));
+
