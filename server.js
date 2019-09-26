e25782cd66ea07e08e9f22214bf5a429c706b1d7
Robert Wise <robcodehub@gmail.com>
2019-09-26 11:21:18 +0700

    added first feature page

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/feature1', (req, res, next) => {
+  res.sendFule(path.join(__dirname, 'feature1.html');
+})
+
+
 const port = process.env.PORT || 3005;
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
